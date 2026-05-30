f89e8ec843782999a4033fe602b5d99d30cf46f4
Александра Бодрова <m2403413@edu.misis.ru>
2026-05-30 17:10:00 +0300

    Добавила DTO и валидацию пользователей

---
+++ backend/src/users/dto/update-user.dto.ts
@@ -0,0 +1,29 @@
+import { IsEmail, IsIn, IsInt, IsOptional, IsString, Length, Max, Min } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class UpdateUserDto {
+  @IsOptional()
+  @IsString()
+  @Length(2, 120)
+  name?: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @Max(120)
+  age?: number;
+
+  @IsOptional()
+  @IsIn(['admin', 'librarian', 'reader', 'guest'])
+  role?: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(6, 80)
+  password?: string;
+}
